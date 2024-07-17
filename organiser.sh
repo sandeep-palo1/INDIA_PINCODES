e0c10646e8559507ee4aafb33fbf87ba97af7fc3
sandypalo <sandeep.palo@onsite.co.in>
2024-07-17 21:40:21 +0530

    added organiser script to process shp fragments

---
+++ organiser.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Concatenate split zip files into a single zip file
+cat india_pincodes.shp.z01 india_pincodes.shp.z02 india_pincodes.shp.z03 india_pincodes.shp.z04 india_pincodes.shp.z05 india_pincodes.shp.zip > india_pincodes_full.shp.zip
+
+# Unzip the combined zip file
+unzip india_pincodes_full.shp.zip
+
+# Unzip any other related zip files (if necessary)
+unzip india_pincodes.cpg.zip
+unzip india_pincodes.prj.zip
+unzip india_pincodes.sbx.zip
+unzip india_pincodes.dbf.zip
+unzip india_pincodes.sbn.zip
+unzip india_pincodes.shx.zip
+
+# Converting to GeoJSON
+# ogr2ogr -f GeoJSON india_pincodes_full.geojson india_pincodes_full.shp
